3d9eabe984551ca13de071f28d5e0b45dad3f209
Rodolfo1924 <rodolfoisaac241904@gmail.com>
2025-05-19 18:56:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
@@ -7,7 +8,13 @@ import io
 
 # ----- Inicializar Flask -----
 app = Flask(__name__)
+CORS(app)
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    # tu lógica para manejar la imagen y predecir
+    return jsonify({'result': 'Predicción aquí'})
+    
 # ----- Definir modelo -----
 class EmotionCNN(nn.Module):
     def __init__(self, num_classes):
