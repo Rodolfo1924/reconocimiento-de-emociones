d723fe68c6bd919cb3083d3a54625ee73257f5d4
Rodolfo1924 <rodolfoisaac241904@gmail.com>
2025-05-22 19:24:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 
 # Inicializa app
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
