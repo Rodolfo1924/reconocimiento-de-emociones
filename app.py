4521d634f63d43650ce7e316bfddc1bac406b8e2
Rodolfo1924 <rodolfoisaac241904@gmail.com>
2025-05-25 11:55:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ class NanoEmotionCNN(nn.Module):
 
 # Carga directa del modelo ya subido (NO usar gdown en Render Free)
 model = NanoEmotionCNN(num_classes=len(clases)).to(device)
-model.load_state_dict(torch.load("emotion_model_ligero3.pth", map_location=device))
+model.load_state_dict(torch.load("emotion_model_ligero4.pth", map_location=device))
 model.eval()
 
 # Transformación de imagen
