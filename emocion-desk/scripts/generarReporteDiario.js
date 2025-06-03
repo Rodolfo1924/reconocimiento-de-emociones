ee84e8267dc2f919a7fadfd4380579b24d679ac5
Rodolfo1924 <rodolfoisaac241904@gmail.com>
2025-06-03 13:40:04 -0600

    Delete emocion-desk directory

--- emocion-desk/scripts/generarReporteDiario.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const { generarReporte } = require(path.join(__dirname, '../utils/report'));
-
-async function main() {
-  await generarReporte();
-  process.exit(0);
-}
-
-main();
